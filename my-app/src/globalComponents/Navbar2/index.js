d2a17b486f745b9de01ad25676c97c66a4b523ab
Colin Whitcomb <colinsw@berkeley.edu>
2020-09-27 13:30:06 -0700

    [add] fake navbar.

---
+++ my-app/src/globalComponents/Navbar2/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function NavBar2() {
+    return (
+    <div class="nav-content">
+        <nav>
+        <ul class="tabs tabs-transparent">
+        <li class="tab"><a href="#test1">Home</a></li>
+        <li class="tab"><a href="#test2">Apparel &amp; Textiles </a></li>
+        <li class="tab"><a href="#test3">Signs &amp; Paintings </a></li>
+        <li class="tab"><a href="#test4">Home Decor</a></li>
+        <li class="tab"><a href="#test5">About the Studio</a></li>
+        <li class="tab"><a href="#test6">Contact</a></li>
+      </ul>
+          </nav>
+    </div>
+        )
+}
--- my-app/src/pages/Home/page.js
+++ my-app/src/pages/Home/page.js
@@ -2,16 +2,22 @@ import React from 'react'
 import HomeP1 from '../../utils/images/HomeP1.png'
 import HomeP2 from '../../utils/images/HomeP2.png'
 import HomeP3 from '../../utils/images/HomeP3.png'
+import Logo2 from '../../utils/images/Logo2.png'
+import NavBar2 from '../../globalComponents/Navbar2/index'
 
 export default function HomePage() {
     return( 
-        <div id="bg2">
-            <div className='container'>
-                <div className='row'>
-                    <img src={HomeP1} className='responsive-image col s4' id="homeImg1" alt='Picture of arts and crafts'></img>
-                    <img src={HomeP2} className='responsive-image col s4' id="homeImg1" alt='Picture of arts and crafts'></img>
-                    <img src={HomeP3} className='responsive-image col s4' id="homeImg1" alt='Picture of arts and crafts'></img>
-                    <img src={HomeP3} className='responsive-image col s4' id="homeImg1" alt='Picture of arts and crafts'></img>
+        <div>
+            <img src={Logo2} className='responsive-image col s4'  alt='Picture of arts and crafts'></img>
+            <NavBar2 />
+                <div id="bg2">
+                    <div className='container'>
+                        <div className='row'>
+                            <img src={HomeP1} className='responsive-image col s4' id="homeImg1" alt='Picture of arts and crafts'></img>
+                            <img src={HomeP2} className='responsive-image col s4' id="homeImg1" alt='Picture of arts and crafts'></img>
+                            <img src={HomeP3} className='responsive-image col s4' id="homeImg1" alt='Picture of arts and crafts'></img>
+                            <img src={HomeP3} className='responsive-image col s4' id="homeImg1" alt='Picture of arts and crafts'></img>
+                        </div>
                     </div>
                 </div>
         </div>

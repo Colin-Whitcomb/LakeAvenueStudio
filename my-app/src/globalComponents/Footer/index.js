73b6a7a6b0df244823f370a14f8736a1876d8485
Colin Whitcomb <colinsw@berkeley.edu>
2020-09-27 13:54:40 -0700

    [add] footer and styling.

---
+++ my-app/src/globalComponents/Footer/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default function Footer() {
+    return (
+        <footer class="page-footer">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">Contact Info</h5>
+                <p class="grey-text text-lighten-4">Make sure to follow us on Instagram and Facebook!</p>
+              </div>
+              
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            © 2020 Copyright
+            <a class="grey-text text-lighten-4 right" href="#!">Link</a>
+            </div>
+          </div>
+        </footer>
+    )
+
+}
--- my-app/src/pages/Home/page.js
+++ my-app/src/pages/Home/page.js
@@ -2,24 +2,28 @@ import React from 'react'
 import HomeP1 from '../../utils/images/HomeP1.png'
 import HomeP2 from '../../utils/images/HomeP2.png'
 import HomeP3 from '../../utils/images/HomeP3.png'
+import HomeP4 from '../../utils/images/HomeP4.png'
 import Logo2 from '../../utils/images/Logo2.png'
 import NavBar2 from '../../globalComponents/Navbar2/index'
+import Footer from '../../globalComponents/Footer/index'
+import './style.css'
 
 export default function HomePage() {
     return( 
         <div>
-            <img src={Logo2} className='responsive-image col s4'  alt='Picture of arts and crafts'></img>
+            <img src={Logo2} className='responsive-image col s4'  alt='Picture of arts and crafts' id="homeImg1"></img>
             <NavBar2 />
                 <div id="bg2">
                     <div className='container'>
                         <div className='row'>
                             <img src={HomeP1} className='responsive-image col s4' id="homeImg1" alt='Picture of arts and crafts'></img>
-                            <img src={HomeP2} className='responsive-image col s4' id="homeImg1" alt='Picture of arts and crafts'></img>
-                            <img src={HomeP3} className='responsive-image col s4' id="homeImg1" alt='Picture of arts and crafts'></img>
-                            <img src={HomeP3} className='responsive-image col s4' id="homeImg1" alt='Picture of arts and crafts'></img>
+                            <img src={HomeP2} className='responsive-image col s4'  id="homeImg1" alt='Picture of arts and crafts'></img>
+                            <img src={HomeP3} className='responsive-image col s4'  id="homeImg1" alt='Picture of arts and crafts'></img>
+                            <img src={HomeP4} className='responsive-image col s8'  id="homeImg1" alt='Picture of arts and crafts'></img>
                         </div>
                     </div>
                 </div>
+                <Footer />
         </div>
     )
 }

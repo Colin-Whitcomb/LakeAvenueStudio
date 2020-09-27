85c78a578485e054b7da4f9fdadc4aadc033a372
Colin Whitcomb <colinsw@berkeley.edu>
2020-09-27 10:08:32 -0700

    [add] beginning of LtH btn.

---
+++ my-app/src/globalComponents/LandtoHomeBtn/index.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
+import Landing from '../../pages/Landing/index'
+import Home from '../../pages/Home/index'
+
+// These names need to line up in the Switch
+function LandingPage() {
+    return (
+      <Landing/> 
+    )
+  }
+  
+  function HomePage() {
+    return (
+      <Home /> 
+    )
+  }
+  
+  function Users() {
+    return <h2>Users</h2>;
+  }
+  
+  // what we are exporting on this file
+  export default function LtHBtn() {
+    return (
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/home">Home</Link>
+              </li>
+
+            </ul>
+          </nav>
+  
+          {/* A <Switch> looks through its children <Route>s and
+              renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/home">
+              <HomePage />
+            </Route>
+            <Route path="/users">
+              <Users />
+            </Route>
+            <Route path="/">
+              <LandingPage />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+  
--- my-app/src/pages/Landing/page.js
+++ my-app/src/pages/Landing/page.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import logo from '../../utils/images/LASlogo1.png'
 import productPic from '../../utils/images/LandImg1.png'
+// import LtHBtn from '../../globalComponents/LandtoHomeBtn/index'
 
 export default function LandingPage() {
     return( 
         <div>
             <div id="bg">
-              <img src={logo} className='responsive-image Logo1'></img>
-              {/* <img src={productPic} className='responsive-image' id="landImg1"></img> */}
+              {/* <LtHBtn /> */}
+              <img src={logo} className='responsive-image Logo1' alt='Lake Avenue Studio Logo'></img>
+              <img src={productPic} className='responsive-image' id="landImg1" alt='Picture of arts and crafts'></img>
             </div>
             {/* <img className="responsive-img" id="Logo1" src={logo} alt="Lake Avenue Studio Logo"></img> */}
         </div>
